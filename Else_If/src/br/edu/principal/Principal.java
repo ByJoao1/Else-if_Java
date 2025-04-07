3d99bca05a3faf46713d96b15c2ee9a7988affa4
João Pedro <141186203+ByJoao1@users.noreply.github.com>
2025-04-07 14:01:49 -0500

    Update Principal.java

--- Else_If/src/br/edu/principal/Principal.java
+++ Else_If/src/br/edu/principal/Principal.java
@@ -1,10 +1,54 @@
 package br.edu.principal;
 
-public class Principal {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+	public class Principal {
+		public static void main(String[] args) {
+		        
+		        Scanner scanner = new Scanner(System.in);
+		        System.out.print("Digite um numero de 1 a 12: ");
+		        int mês=scanner.nextInt();
+		   
+		       
+		         
+		        if (mês == 1) {
+		        	System.out.print("janeiro ");
+		        }
+		        else if (mês == 2) {
+		        	System.out.print("Fevereiro");
+		        }
+		        else if (mês == 3) {
+		        	System.out.print("Março");
+		        }
+		        else if (mês == 4) {
+		        	System.out.print("Abril");
+		        }
+		        else if (mês == 5) {
+		        	System.out.print("maio");
+		        }
+		        else if (mês == 6) {
+		        	System.out.print("junho");
+		        }
+		        else if (mês == 7) {
+		        	System.out.print("julho");
+		        }
+		        else if (mês == 8) {
+		        	System.out.print("agosto");
+		        }
+		        else if (mês == 9) {
+		        	System.out.print("setembro");
+		        }
+		        else if (mês == 10) {
+		        	System.out.print("outobro");
+		        }
+		        else if (mês == 11) {
+		        	System.out.print("novembro");
+		        }
+		        else if (mês == 12) {
+		        	System.out.print("dezembro");
+		        }
+		        else {
+		        	System.out.print("Numero invalido");
+		        }
+		    }
+		}
